92bec871439919d12983a51a1b5601aa8dbbf065
Donna <donna@letsplax.com>
2021-09-10 18:49:18 +0900

    gotta go

---
+++ state_test/lib/product.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String contents;
+
+  Product({required this.id, required this.title, required this.contents});
+}
