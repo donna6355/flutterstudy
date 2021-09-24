ec2f36275b41ea69cc7c1234d352976e24f8f7e9
Donna <donnajun1214@gmail.com>
2021-09-24 15:33:34 +0900

    fix but still doesnt work in ios

--- native_practice/ios/Runner/AppDelegate.swift
+++ native_practice/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBM25zXl6dZAWZhp-1jfwkrSdJKX5nB7GY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
