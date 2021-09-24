d97e3f701be9148e27b2ab33faf01cc828414858
Donna <donnajun1214@gmail.com>
2021-09-24 11:11:31 +0900

    sql fetch data

--- native_practice/lib/helpers/db_helper.dart
+++ native_practice/lib/helpers/db_helper.dart
@@ -2,13 +2,22 @@ import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<sql.Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(path.join(dbPath, 'places.db'),
+    return sql.openDatabase(path.join(dbPath, 'places.db'),
         onCreate: (db, version) {
       return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT');
+          'CREATE TABLE places(id TEXT PRIMARY KEY, title TEXT, image TEXT');
     }, version: 1);
-    await sqlDb.insert(table, data, conflictAlgorithm: sql.ConflictAlgorithm.replace )
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(table, data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
--- native_practice/lib/providers/great_places.dart
+++ native_practice/lib/providers/great_places.dart
@@ -4,9 +4,9 @@ import './place.dart';
 import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
-  List<Place> _items = [];
+  List<dynamic> _items = [];
 
-  List<Place> get items {
+  List<dynamic> get items {
     return [..._items];
   }
 
@@ -23,5 +23,17 @@ class GreatPlaces with ChangeNotifier {
       'title': newPlace.title,
       'image': newPlace.image.path,
     });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('places');
+    _items = dataList.map((item) {
+      Place(
+        id: item['id'],
+        title: item['title'],
+        image: File(item['image']),
+      );
+    }).toList();
+    notifyListeners();
   }
 }
