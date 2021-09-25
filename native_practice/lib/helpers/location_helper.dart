35c5ae000f04c50d9fd3e77fda9f7454bfd5fc7c
Donna <donnajun1214@gmail.com>
2021-09-25 14:36:08 +0900

    geocoding

--- native_practice/lib/helpers/location_helper.dart
+++ native_practice/lib/helpers/location_helper.dart
@@ -1,8 +1,19 @@
-const GOOGLE_KEY = 'AIzaSyBM25zXl6dZAWZhp-1jfwkrSdJKX5nB7GY';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const GOOGLE_KEY = 'AIzaSyDlGjUkwzDes0HYlsKpgqtjHLawnEZ0c5s';
 
 class LocationHelper {
   static String genLocPrevImg(
       {required double latitude, required double longitude}) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_KEY';
+  }
+
+  static Future<String> getAddress(double lat, double lng) async {
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_KEY');
+    final response = await http.get(url);
+    print(json.decode(response.body));
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
