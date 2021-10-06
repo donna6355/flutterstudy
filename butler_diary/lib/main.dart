204d83d6433f6ad8b5cd472346bf9bcaf184c4bf
Donna <donnajun1214@gmail.com>
2021-10-06 12:21:36 +0900

    init hive

--- butler_diary/lib/main.dart
+++ butler_diary/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(MyApp());
 }
 
