5e0d462c0013ed3e41dc7d8db459f6cdc495a6b5
Donna <donnajun1214@gmail.com>
2021-10-10 16:59:13 +0900

    drawer draft

--- butler_diary/lib/widget/drawer_profile.dart
+++ butler_diary/lib/widget/drawer_profile.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../model/profile.dart';
 
 class DrawerProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: [
-          Text('냥님1'),
-          Text('냥님2'),
-          Text('냥님3'),
-          Text('이용 안내'),
-          Text('개발자 메시지'),
+          Expanded(
+            child: ValueListenableBuilder<Box>(
+                valueListenable: Hive.box('myCat').listenable(),
+                builder: (context, box, widget) {
+                  return ListView.builder(
+                    itemBuilder: (context, idx) {
+                      final Profile profile = box.getAt(idx);
+                      return TextButton(
+                          onPressed: () {}, child: Text(profile.name));
+                    },
+                    itemCount: box.length,
+                  );
+                }),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('이용 안내'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('개발자 메시지'),
+          ),
         ],
       ),
     );
