b474479176d62efaed209081aa21bd18f20c09b2
Donna <donnajun1214@gmail.com>
2021-10-21 16:20:48 +0900

    icon in diary card

--- butler_diary/lib/widget/diary_card.dart
+++ butler_diary/lib/widget/diary_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../model/diary.dart';
+import './multi_diary_icon.dart';
 
 class DiaryCard extends StatelessWidget {
   final Diary dailyData;
@@ -36,32 +37,42 @@ class DiaryCard extends StatelessWidget {
               child: Text(dailyData.date),
               width: double.infinity,
             ),
-            Wrap(
-              children: [
-                if (dailyData.dryFood != -1) Text('dryFood'),
-                if (dailyData.wetFood != -1) Text('wetFood'),
-                if (dailyData.water != -1) Text('water'),
-                if (dailyData.waterySnack) Text('waterySnack'),
-                if (dailyData.drySnack) Text('drySnack'),
-                if (dailyData.snack) Text('snack'),
-                if (dailyData.pee != -1) Text('pee'),
-                if (dailyData.poo != -1) Text('poo'),
-                if (dailyData.hairBall) Text('hairBall'),
-                if (dailyData.diarrhea) Text('diarrhea'),
-                if (dailyData.vomit) Text('vomit'),
-                if (dailyData.destroy) Text('destroy'),
-                if (dailyData.vet) Text('vet'),
-                if (dailyData.vaccine) Text('vaccine'),
-                if (dailyData.pill) Text('pill'),
-                if (dailyData.eyeDrop) Text('eyeDrop'),
-                if (dailyData.hunting) Text('hunting'),
-                if (dailyData.brushTeeth) Text('brushTeeth'),
-                if (dailyData.brushFur) Text('brushFur'),
-                if (dailyData.bath) Text('bath'),
-                if (dailyData.toilet) Text('toilet'),
-                if (dailyData.note.isNotEmpty) Text(dailyData.note),
-                if (dailyData.photos.length > 0) Text('photos'),
-              ],
+            Container(
+              width: double.infinity,
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                children: [
+                  if (dailyData.dryFood != -1)
+                    MultiDiaryIcon(dailyData.dryFood, 'tbd', '습식 실타냥', '습식 쪼오금',
+                        '습식 적당히', '습식 왕창!'),
+                  if (dailyData.wetFood != -1)
+                    MultiDiaryIcon(dailyData.wetFood, 'tbd', '건식 실타냥', '건식 쪼오금',
+                        '건식 적당히', '건식 왕창!'),
+                  if (dailyData.water != -1)
+                    MultiDiaryIcon(dailyData.water, 'tbd', '물은 실타냥', '사알짝 할짝',
+                        '하알짝 할짝', '물먹는 하마'),
+                  if (dailyData.waterySnack) Text('waterySnack'),
+                  if (dailyData.drySnack) Text('drySnack'),
+                  if (dailyData.snack) Text('snack'),
+                  if (dailyData.pee != -1) Text('pee'),
+                  if (dailyData.poo != -1) Text('poo'),
+                  if (dailyData.hairBall) Text('hairBall'),
+                  if (dailyData.diarrhea) Text('diarrhea'),
+                  if (dailyData.vomit) Text('vomit'),
+                  if (dailyData.destroy) Text('destroy'),
+                  if (dailyData.vet) Text('vet'),
+                  if (dailyData.vaccine) Text('vaccine'),
+                  if (dailyData.pill) Text('pill'),
+                  if (dailyData.eyeDrop) Text('eyeDrop'),
+                  if (dailyData.hunting) Text('hunting'),
+                  if (dailyData.brushTeeth) Text('brushTeeth'),
+                  if (dailyData.brushFur) Text('brushFur'),
+                  if (dailyData.bath) Text('bath'),
+                  if (dailyData.toilet) Text('toilet'),
+                  if (dailyData.note.isNotEmpty) Text(dailyData.note),
+                  if (dailyData.photos.length > 0) Text('photos'),
+                ],
+              ),
             ),
           ],
         ),
---
+++ butler_diary/lib/widget/multi_diary_icon.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MultiDiaryIcon extends StatelessWidget {
+  final double value;
+  final String label1;
+  final String label2;
+  final String label3;
+  final String label4;
+  final String keyword;
+
+  MultiDiaryIcon(this.value, this.keyword, this.label1, this.label2,
+      this.label3, this.label4);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 50,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        children: [
+          Icon(Icons.add),
+          if (value == 0)
+            Text(
+              label1,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          if (value == 1)
+            Text(
+              label2,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          if (value == 2)
+            Text(
+              label3,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          if (value == 3)
+            Text(
+              label4,
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
