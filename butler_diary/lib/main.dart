6ad7c7428f789777b59ba88d45f0f01980c48a93
Donna <donnajun1214@gmail.com>
2021-10-26 09:42:08 +0900

    lock screen orientation

--- butler_diary/lib/main.dart
+++ butler_diary/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:flutter/services.dart';
 
 import './screen/profile_edit_scr.dart';
 import './screen/living_room.dart';
@@ -13,6 +14,7 @@ import './screen/profile_scr.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final appDocDir = await path_provider.getApplicationDocumentsDirectory();
   await Hive.initFlutter(appDocDir.path);
   Hive.registerAdapter(DiaryAdapter());
