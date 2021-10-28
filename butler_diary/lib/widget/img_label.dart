356c153a59afb89ae2ece91f2836cafc38c3992f
Donna <donnajun1214@gmail.com>
2021-10-28 17:27:08 +0900

    better with border

---
+++ butler_diary/lib/widget/img_label.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ImgLabel extends StatelessWidget {
+  final String label;
+  final String choice;
+  final double choiceVal;
+  final double idx;
+  ImgLabel(this.label, this.choice, this.choiceVal, this.idx);
+
+  @override
+  Widget build(BuildContext context) {
+    String _findImgName(int num) {
+      String fileName = '';
+
+      switch (label) {
+        case '습식 실타냥':
+          fileName = 'wetf';
+          break;
+        case '건식 실타냥':
+          fileName = 'dryf';
+          break;
+        case '물 실타냥':
+          fileName = 'water';
+          break;
+        case '변비냥':
+          fileName = 'poo';
+          break;
+        case '으잉?':
+          fileName = 'pee';
+          break;
+        default:
+          break;
+      }
+
+      return 'asset/icon/$fileName$num.png';
+    }
+
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: choiceVal == idx
+            ? Border.all(color: Color(0xff454442), width: 1)
+            : Border.all(color: Color(0xffE5E4DB), width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            width: 54,
+            height: 54,
+            child: Image.asset(_findImgName(1)),
+          ),
+          // Icon(
+          //   Icons.add,
+          //   color: choiceVal == 1 ? Color(0xff454442) : Color(0xffB7B6AF),
+          // ),
+          Text(
+            choice,
+            style: TextStyle(
+              fontSize: 12,
+              color: choiceVal == idx ? Color(0xff454442) : Color(0xff73726e),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- butler_diary/lib/widget/multiple_choice.dart
+++ butler_diary/lib/widget/multiple_choice.dart
@@ -48,114 +48,150 @@ class MultipleChoice extends StatelessWidget {
         children: [
           GestureDetector(
             onTap: () => valueState(choice1, 0.0),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: choiceVal == 0
+                    ? Border.all(color: Color(0xff454442), width: 1)
+                    : Border.all(color: Color(0xffE5E4DB), width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    width: 54,
+                    height: 54,
+                    child: Image.asset(_findImgName(0)),
                   ),
-                  width: 54,
-                  height: 54,
-                  child: Image.asset(_findImgName(0)),
-                ),
-                // Icon(
-                //   Icons.add,
-                //   color: choiceVal == 0 ? Color(0xff454442) : Color(0xffB7B6AF),
-                // ),
-                Text(
-                  choice1,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color:
-                        choiceVal == 0 ? Color(0xff454442) : Color(0xff73726e),
+                  Text(
+                    choice1,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: choiceVal == 0
+                          ? Color(0xff454442)
+                          : Color(0xff73726e),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           GestureDetector(
             onTap: () => valueState(choice1, 1.0),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: choiceVal == 1
+                    ? Border.all(color: Color(0xff454442), width: 1)
+                    : Border.all(color: Color(0xffE5E4DB), width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    width: 54,
+                    height: 54,
+                    child: Image.asset(_findImgName(1)),
                   ),
-                  width: 54,
-                  height: 54,
-                  child: Image.asset(_findImgName(1)),
-                ),
-                // Icon(
-                //   Icons.add,
-                //   color: choiceVal == 1 ? Color(0xff454442) : Color(0xffB7B6AF),
-                // ),
-                Text(
-                  choice2,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color:
-                        choiceVal == 1 ? Color(0xff454442) : Color(0xff73726e),
+                  // Icon(
+                  //   Icons.add,
+                  //   color: choiceVal == 1 ? Color(0xff454442) : Color(0xffB7B6AF),
+                  // ),
+                  Text(
+                    choice2,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: choiceVal == 1
+                          ? Color(0xff454442)
+                          : Color(0xff73726e),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           GestureDetector(
             onTap: () => valueState(choice1, 2.0),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: choiceVal == 2
+                    ? Border.all(color: Color(0xff454442), width: 1)
+                    : Border.all(color: Color(0xffE5E4DB), width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    width: 54,
+                    height: 54,
+                    child: Image.asset(_findImgName(2)),
                   ),
-                  width: 54,
-                  height: 54,
-                  child: Image.asset(_findImgName(2)),
-                ),
-                // Icon(
-                //   Icons.add,
-                //   color: choiceVal == 2 ? Color(0xff454442) : Color(0xffB7B6AF),
-                // ),
-                Text(
-                  choice3,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color:
-                        choiceVal == 2 ? Color(0xff454442) : Color(0xff73726e),
+                  // Icon(
+                  //   Icons.add,
+                  //   color: choiceVal == 2 ? Color(0xff454442) : Color(0xffB7B6AF),
+                  // ),
+                  Text(
+                    choice3,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: choiceVal == 2
+                          ? Color(0xff454442)
+                          : Color(0xff73726e),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           GestureDetector(
             onTap: () => valueState(choice1, 3.0),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: choiceVal == 3
+                    ? Border.all(color: Color(0xff454442), width: 1)
+                    : Border.all(color: Color(0xffE5E4DB), width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    width: 54,
+                    height: 54,
+                    child: Image.asset(_findImgName(3)),
                   ),
-                  width: 54,
-                  height: 54,
-                  child: Image.asset(_findImgName(3)),
-                ),
-                // Icon(
-                //   Icons.add,
-                //   color: choiceVal == 3 ? Color(0xff454442) : Color(0xffB7B6AF),
-                // ),
-                Text(
-                  choice4,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color:
-                        choiceVal == 3 ? Color(0xff454442) : Color(0xff73726e),
+                  // Icon(
+                  //   Icons.add,
+                  //   color: choiceVal == 3 ? Color(0xff454442) : Color(0xffB7B6AF),
+                  // ),
+                  Text(
+                    choice4,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: choiceVal == 3
+                          ? Color(0xff454442)
+                          : Color(0xff73726e),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
