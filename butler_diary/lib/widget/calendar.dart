4e9a9d1d195d7be274e8869ca88642e32c56ba4e
Donna <donnajun1214@gmail.com>
2021-11-04 15:22:25 +0900

    calendar format btn

--- butler_diary/lib/widget/calendar.dart
+++ butler_diary/lib/widget/calendar.dart
@@ -13,62 +13,79 @@ class _CalendarState extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      firstDay: DateTime(2000),
-      lastDay: DateTime(DateTime.now().year + 1),
-      focusedDay: _focused,
-      locale: 'ko_KR',
-      selectedDayPredicate: (day) {
-        return isSameDay(_selected, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selected = selectedDay;
-          _focused = focusedDay; // update `_focusedDay` here as well
-        });
-      },
-      calendarFormat: _calendarFormat,
-      onFormatChanged: (format) {
-        if (_calendarFormat == format) return;
-        setState(() {
-          if (_calendarFormat == CalendarFormat.week)
-            _calendarFormat = CalendarFormat.month;
-          else if (_calendarFormat == CalendarFormat.month)
-            _calendarFormat = CalendarFormat.week;
-        });
-      },
-      calendarStyle: CalendarStyle(
-        todayDecoration: const BoxDecoration(
-          color: const Color(0xffa0a099),
-          shape: BoxShape.circle,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        TableCalendar(
+          firstDay: DateTime(2000),
+          lastDay: DateTime(DateTime.now().year + 1),
+          focusedDay: _focused,
+          locale: 'ko_KR',
+          selectedDayPredicate: (day) {
+            return isSameDay(_selected, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selected = selectedDay;
+              _focused = focusedDay; // update `_focusedDay` here as well
+            });
+          },
+          calendarFormat: _calendarFormat,
+          // onFormatChanged: (format) {
+          //   if (_calendarFormat == format) return;
+          //   setState(() {
+          //     if (_calendarFormat == CalendarFormat.week)
+          //       _calendarFormat = CalendarFormat.month;
+          //     else if (_calendarFormat == CalendarFormat.month)
+          //       _calendarFormat = CalendarFormat.week;
+          //   });
+          // },
+          calendarStyle: CalendarStyle(
+            todayDecoration: const BoxDecoration(
+              color: const Color(0xffa0a099),
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: const BoxDecoration(
+              color: const Color(0xff454442),
+              shape: BoxShape.circle,
+            ),
+            selectedTextStyle: const TextStyle(
+              color: const Color(0xFFFAFAFA),
+              fontSize: 15.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // eventLoader: (day) {
+          //   return _getEventsForDay(day);
+          // },
+          headerStyle: HeaderStyle(
+            titleCentered: true,
+            formatButtonVisible: false,
+            //better to make custom formatbtn
+            // formatButtonTextStyle: TextStyle(
+            //   color: const Color(0xfffafafa),
+            // ),
+            // formatButtonDecoration: const BoxDecoration(
+            //   color: const Color(0xffa0a099),
+            //   borderRadius: BorderRadius.all(
+            //     Radius.circular(15),
+            //   ),
+            // ),
+          ),
         ),
-        selectedDecoration: const BoxDecoration(
-          color: const Color(0xff454442),
-          shape: BoxShape.circle,
+        TextButton(
+          onPressed: () {
+            setState(() {
+              if (_calendarFormat == CalendarFormat.week)
+                _calendarFormat = CalendarFormat.month;
+              else if (_calendarFormat == CalendarFormat.month)
+                _calendarFormat = CalendarFormat.week;
+            });
+          },
+          child: Text(
+              '${_calendarFormat == CalendarFormat.week ? '월간' : '주간'} 달력보기'),
         ),
-        selectedTextStyle: const TextStyle(
-          color: const Color(0xFFFAFAFA),
-          fontSize: 15.0,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      // eventLoader: (day) {
-      //   return _getEventsForDay(day);
-      // },
-      headerStyle: HeaderStyle(
-        titleCentered: true,
-        formatButtonVisible: false,
-        //better to make custom formatbtn
-        // formatButtonTextStyle: TextStyle(
-        //   color: const Color(0xfffafafa),
-        // ),
-        // formatButtonDecoration: const BoxDecoration(
-        //   color: const Color(0xffa0a099),
-        //   borderRadius: BorderRadius.all(
-        //     Radius.circular(15),
-        //   ),
-        // ),
-      ),
+      ],
     );
   }
 }
