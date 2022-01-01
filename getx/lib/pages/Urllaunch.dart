0a2fbfcea0918c7f8d34602fe14003f5cd669e59
Donna <donnajun1214@gmail.com>
2022-01-01 15:03:27 +0900

    str to list to widget

--- getx/lib/pages/Urllaunch.dart
+++ getx/lib/pages/Urllaunch.dart
@@ -2,11 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:get/get.dart';
 
 class Urllaunch extends StatelessWidget {
-  void _launchUrl() async {
+  void _launchUrl(String str) async {
     //weird....
-    const url = 'https://pub.dev/';
+    String url = str.isEmpty ? 'https://pub.dev/' : str.trim();
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -14,15 +15,79 @@ class Urllaunch extends StatelessWidget {
     }
   }
 
+  Widget generateWg(String str) {
+    if (str.contains('http')) {
+      return TextButton(
+          onPressed: () {
+            _launchUrl(str);
+          },
+          child: Text(str.trim()));
+    } else {
+      return Text(str.trim());
+    }
+  }
+
+  List<Widget> _stringToText() {
+    const String string =
+        'Happy New Year\n Seasons Greeting\n https://www.youtube.com/watch?v=hXgMMo8p1ig';
+    // List<dynamic> result = string.split('\n');
+
+    List<String> result = string.split('\n');
+    List<Widget> widgets = [];
+    for (final str in result) {
+      if (str.contains('http')) {
+        widgets.add(
+          GestureDetector(
+            onTap: () {
+              _launchUrl(str);
+            },
+            child: Text(
+              str.trim(),
+              style: TextStyle(
+                color: Colors.amber,
+              ),
+            ),
+          ),
+        );
+      } else {
+        widgets.add(Text(str.trim()));
+      }
+    }
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            _launchUrl();
-          },
-          child: Text('go to flutter.dev'),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _launchUrl('');
+              },
+              child: Text('go to flutter.dev'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _stringToText();
+              },
+              child: Text('str to widget'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.toNamed('/third/HappyNewYear?date=Jan1');
+              },
+              child: Text('go to third'),
+            ),
+            Column(
+              children: _stringToText(),
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )
+          ],
         ),
       ),
     );
