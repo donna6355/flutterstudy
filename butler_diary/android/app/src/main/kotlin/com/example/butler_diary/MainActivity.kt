a6dd0db62f372c33c4577658f4f76d4a5e0bfe52
Donna <donnajun1214@gmail.com>
2022-06-30 08:43:21 +0900

    [add] disable screenshot for both and and ios

--- butler_diary/android/app/src/main/kotlin/com/example/butler_diary/MainActivity.kt
+++ butler_diary/android/app/src/main/kotlin/com/example/butler_diary/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.donna6355.butler_diary
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
