fe5d536ab1d57529107d56edba7ff64b141d1633
Donna <donnajun1214@gmail.com>
2022-07-13 09:14:04 +0900

    test sorting map list

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:tutorial/map_test.dart';
 import 'firebase_options.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -137,6 +138,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Expanded(child: MapTest()),
           ],
         ),
       ),
---
+++ tutorial/lib/map_test.dart
@@ -0,0 +1,117 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+class MapTest extends StatefulWidget {
+  const MapTest({Key? key}) : super(key: key);
+
+  @override
+  State<MapTest> createState() => _MapTestState();
+}
+
+class _MapTestState extends State<MapTest> {
+  final List<Map<String, dynamic>> rawData = [
+    {
+      'machineId': 'alksdjflkajd',
+      'time': DateTime(2020, 7, 1, 15, 20),
+      'location': 'SEOUL',
+    },
+    {
+      'machineId': 'alksdjflas2d',
+      'time': DateTime(2020, 7, 1, 10, 20),
+      'location': 'GAYANG',
+    },
+    {
+      'machineId': 'alksdjflas2d',
+      'time': DateTime(2020, 7, 1, 12, 20),
+      'location': 'GAYANG',
+    },
+    {
+      'machineId': 'alksdjflkajd',
+      'time': DateTime(2020, 7, 2, 15, 20),
+      'location': 'SEOUL',
+    },
+    {
+      'machineId': 'alksdjflkajd',
+      'time': DateTime(2020, 7, 5, 15, 20),
+      'location': 'SEOUL',
+    },
+    {
+      'machineId': 'alksdjflkajd',
+      'time': DateTime(2020, 7, 4, 15, 20),
+      'location': 'SEOUL',
+    },
+    {
+      'machineId': 'wersdjflkajd',
+      'time': DateTime(2020, 7, 4, 15, 20),
+      'location': 'JEJU',
+    },
+    {
+      'machineId': 'wersdjflkajd',
+      'time': DateTime(2020, 7, 7, 19, 20),
+      'location': 'JEJU',
+    },
+  ];
+  List<Widget> _computedList = [];
+
+  void _computed() {
+    List<Widget> returnVal = [];
+    Map<String, List> computed = {};
+    const String keyword = 'machineId';
+    for (var data in rawData) {
+      if (computed[data[keyword]] != null) {
+        computed[data[keyword]]!.add(data);
+      } else {
+        computed[data[keyword]] = [data];
+      }
+    }
+    print(computed);
+
+    final List<String> label = computed.keys.toList()
+      ..sort((a, b) {
+        print(a);
+        print(b);
+        return a.compareTo(b);
+      });
+
+    for (var data in label) {
+      returnVal.add(Text(data));
+      for (var data in computed[data]!) {
+        returnVal.add(_sessionCard(data));
+      }
+    }
+    setState(() {
+      _computedList = returnVal;
+    });
+  }
+
+  final Map<String, dynamic> computed = {};
+  @override
+  Widget build(BuildContext context) {
+    _computed();
+    return ListView(
+      children: _computedList,
+    );
+  }
+
+  Widget _sessionCard(Map<String, dynamic> data) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      color: Colors.blue[100],
+      child: Column(
+        children: [
+          Text(
+            data['machineId'],
+          ),
+          Text(
+            data['location'],
+          ),
+          Text(
+            data['time'].toString(),
+          )
+        ],
+      ),
+    );
+  }
+}
