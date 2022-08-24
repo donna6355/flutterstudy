e40195de9b402e77f28e88b10e0cece410e54b09
Donna <donnajun1214@gmail.com>
2022-08-24 09:01:35 +0900

    singleton serial port module

---
+++ serialport/lib/services/serial_port.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+class Port {
+  static final Port _instance = Port._();
+  static late SerialPort _barcodePort;
+  static late SerialPortReader _barcodeReader;
+
+  Port._() {
+    _barcodePort = SerialPort(SerialPort.availablePorts.last);
+
+    //baudRate = 115200; bits = 8; stopBits = 1;
+
+    _barcodeReader = SerialPortReader(_barcodePort);
+  }
+
+  factory Port() {
+    return _instance;
+  }
+
+  void readBarcodeStream() {
+    if (!_barcodePort.openReadWrite()) {
+      _barcodePort.close();
+    } else {
+      _barcodeReader.stream.listen((data) {
+        print(data);
+        _barcodeReader.close();
+      });
+    }
+  }
+}
