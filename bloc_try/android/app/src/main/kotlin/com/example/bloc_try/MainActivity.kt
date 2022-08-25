612fa7c4b56316bba67cbb980a5cb4fe83eb0803
Donna <donnajun1214@gmail.com>
2022-08-25 09:26:49 +0900

    block/disable screenshot in android

--- bloc_try/android/app/src/main/kotlin/com/example/bloc_try/MainActivity.kt
+++ bloc_try/android/app/src/main/kotlin/com/example/bloc_try/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.bloc_try
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {   
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
