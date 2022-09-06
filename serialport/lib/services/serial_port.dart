4f757a25fb2058d3fe0398b07950e1bcda1e37fc
Donna <donnajun1214@gmail.com>
2022-09-07 08:28:21 +0900

    reference added

--- serialport/lib/services/serial_port.dart
+++ serialport/lib/services/serial_port.dart
@@ -9,6 +9,7 @@ class Port {
   // in case async initializer required... how should I handle?
   //reference
   //https://medium.com/flutter-seoul/dart%EC%97%90%EC%84%9C-singleton-pattern%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-2312616bbb7e
+  //https://daldalhanstory.tistory.com/240
   Port._() {
     _barcodePort = SerialPort(SerialPort.availablePorts.last);
 
