0a666ac01236fedc8e9f9606bb662e9a4de565a2
Donna <donnajun1214@gmail.com>
2022-09-06 10:07:50 +0900

    dio interceptors

--- flavor_test/lib/helpers/http_request.dart
+++ flavor_test/lib/helpers/http_request.dart
@@ -3,36 +3,43 @@ import 'package:dio/dio.dart';
 class BackServer {
   static final BackServer _instance = BackServer._();
   static late Dio _dio;
+
   BackServer._() {
     var options = BaseOptions(
       baseUrl: 'https://www.xx.com/api',
       // connectTimeout: 5000,
       // receiveTimeout: 3000,
     );
-    _dio = Dio(options);
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        options.headers['Authorization'] = 'auth token bulah';
-        // Do something before request is sent
-        return handler.next(options); //continue
-        // If you want to resolve the request with some custom data，
-        // you can resolve a `Response` object eg: `handler.resolve(response)`.
-        // If you want to reject the request with a error message,
-        // you can reject a `DioError` object eg: `handler.reject(dioError)`
-      },
-      // onResponse: (response, handler) {
-      //   // Do something with response data
-      //   return handler.next(response); // continue
-      //   // If you want to reject the request with a error message,
-      //   // you can reject a `DioError` object eg: `handler.reject(dioError)`
-      // },
-      // onError: (DioError e, handler) {
-      //   // Do something with response error
-      //   return handler.next(e); //continue
-      //   // If you want to resolve the request with some custom data，
-      //   // you can resolve a `Response` object eg: `handler.resolve(response)`.
-      // },
-    ));
+
+    // InterceptorsWrapper _defaultInterceptor() => InterceptorsWrapper(
+    //       onRequest: (options, handler) {
+    //         options.queryParameters['lang'] = 'ko'; // add common query params
+    //         handler.next(options);
+    //       },
+    //     );
+    // InterceptorsWrapper _authInterceptor() => InterceptorsWrapper(
+    //       onRequest: (options, handler) {
+    //         options.headers['Authorization'] =
+    //             'auth token bulah'; // add common query params
+    //         handler.next(options);
+    //       },
+    //     );
+
+    // _dio = Dio(options)
+    //   ..interceptors.addAll([
+    //     _authInterceptor(),
+    //     _defaultInterceptor(),
+    //   ]);
+
+    _dio = Dio(options)
+      ..interceptors.add(InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.queryParameters['lang'] = 'ko'; // add common query params
+          options.headers['Authorization'] =
+              'auth token bulah'; // add auth header
+          handler.next(options);
+        },
+      ));
   }
   factory BackServer() {
     return _instance;
