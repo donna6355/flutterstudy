eb080979e2ea67e2c370f3ae0899102c6a4c6a1f
Donna <donnajun1214@gmail.com>
2022-09-08 08:35:09 +0900

    scanner tutorial

--- serialport/macos/Flutter/GeneratedPluginRegistrant.swift
+++ serialport/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_libserialport
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLibserialportPlugin.register(with: registry.registrar(forPlugin: "FlutterLibserialportPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
