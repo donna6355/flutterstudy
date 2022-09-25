39daf4278cf234063a0d333b8995e167020d842e
Donna <donnajun1214@gmail.com>
2022-09-25 16:07:30 +0900

    widget test

--- my_player/test/counter_test.dart
+++ my_player/test/counter_test.dart
@@ -1,7 +1,15 @@
 import '../lib/counter.dart';
+import '../lib/test_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+// UNIT TEST
+// 1. Add the flutter_test dependency
+// 2. Create a test file (=====/lib/counter.dart=====/test/counter_test.dart=====)
+// 3. Create a class to test (lib/counter.dart)
+// 4. Write a test for our class(test/counter_test.dart)
+// 5. Combine multiple tests in a group
+
   test('increment counter test', () {
     final counter = Counter(); // test class, method,... unit!
     counter.increment();
@@ -22,12 +30,32 @@ void main() {
       expect(counter.val, 0);
     });
   });
+
+// ===================================================================
+//WIDGET TEST
+// 1. Add the flutter_test dependency
+// 2. Create a widget to test(=====/lib/test_widget.dart=====)
+// 3. Create a testWidgets test
+// 4. Build the widget using the WidgetTester
+// 5. Search for our widget using a Finder
+// 6. Verify the widget using a Matcher
+
+// 3. Create a testWidgets test
+  testWidgets('this is test for widget test', (WidgetTester tester) async {
+// 4. Build the widget using the WidgetTester
+    await tester
+        .pumpWidget(const TestWidget(title: 'title', message: 'message'));
+
+// 5. Search for our widget using a Finder
+    final titleFinder = find.text('title');
+    final msgFinder = find.text('message');
+
+    //6. Verify the widget using a Matcher
+    expect(titleFinder, findsOneWidget);
+    expect(msgFinder, findsOneWidget);
+
+// [findsNothing], when you want the finder to not find anything.
+// [findsWidgets], when you want the finder to find one or more widgets.
+// [findsNWidgets], when you want the finder to find a specific number of widgets.
+  });
 }
-
-
-// UNIT TEST
-// 1. Add the test dependency
-// 2. Create a test file (=====/lib/counter.dart=====/test/counter_test.dart=====)
-// 3. Create a class to test (lib/counter.dart)
-// 4. Write a test for our class(test/counter_test.dart)
-// 5. Combine multiple tests in a group
