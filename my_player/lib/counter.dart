c4497dfb4dda9b7ffc41bed408fce8769cc0d4fd
Donna <donnajun1214@gmail.com>
2022-10-03 18:57:39 +0900

    updated enum tutorial

--- my_player/lib/counter.dart
+++ my_player/lib/counter.dart
@@ -3,3 +3,19 @@ class Counter {
   int increment() => val++;
   int decrement() => val--;
 }
+
+//updated enum!
+
+enum Test {
+  stopped('Loading is stopped.'),
+
+  loading('Loading is progressing.'),
+
+  complete('Loading is complete.'),
+
+  failed('Loading has failed.');
+
+  final String description;
+  const Test(this.description);
+  bool isFinished() => this == failed || this == complete;
+}
