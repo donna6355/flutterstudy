f1d1d0fa7fb58d49b646fe17bb7510685b63dacd
Donna <donnajun1214@gmail.com>
2022-12-02 09:15:08 +0900

    open close principle... difficult

--- my_player/lib/solid.dart
+++ my_player/lib/solid.dart
@@ -1,20 +1,58 @@
 // https://medium.com/flutter-community/s-o-l-i-d-the-first-5-principles-of-object-oriented-design-with-dart-f31d62135b7e
 
-class Circle {
-  double radius;
-  Circle(this.radius);
+import 'dart:math';
+
+abstract class ShapeInterface {
+  area();
 }
 
-class Square {
+class Circle implements ShapeInterface {
+  double radius;
+  Circle(this.radius);
+
+//after open-closed
+  @override
+  num area() {
+    return pow(radius, 2) * pi;
+  }
+}
+
+class Square implements ShapeInterface {
   int length;
   Square(this.length);
+
+//after open-closed
+  @override
+  num area() {
+    return pow(length, 2);
+  }
 }
 
 class AreaCalculator {
   List<dynamic> shapes;
   AreaCalculator(this.shapes);
 
-  dynamic sum() {}
+  num sum() {
+    //before open-closed
+    // List<num> area = [];
+    // for (var shape in shapes) {
+    //   if (shape is Square) {
+    //     area.add(pow(shape.length, 2));
+    //   } else if (shape is Circle) {
+    //     area.add(pow(shape.radius, 2) * pi);
+    //   }
+    // }
+    // return area.reduce((x, y) => x + y);
+    num total = 0;
+    for (var shape in shapes) {
+      if (shape is ShapeInterface) {
+        total += shape.area();
+        continue;
+      }
+      throw Exception('Shape Exception');
+    }
+    return total;
+  }
 
   dynamic output() {
     return "The output is ${sum()}";
