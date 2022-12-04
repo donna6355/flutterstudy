9235afa01c22b4597226e6f62bcbb2d5d3fce057
Donna <donnajun1214@gmail.com>
2022-12-04 20:02:14 +0900

    implements and with...

--- my_player/lib/inheritance.dart
+++ my_player/lib/inheritance.dart
@@ -14,12 +14,12 @@ class Car extends Vehicle {
   String get definition => 'Car ${super.definition}';
 }
 
+//  You use extends when you want to create a more specific version of a class.
 class Hatch extends Car {
   Hatch(String color) : super(color);
 }
 
-//  You use extends when you want to create a more specific version of a class.
-
+//U use implements when you want to inherit only part not all of them as interface.
 class Car2 implements Vehicle {
   Car2(this.carColor);
 
@@ -43,3 +43,34 @@ main() {
   print('Result: is Vehicle type: ${car is Vehicle}');
   // Output -> is Vehicle type: true
 }
+
+// Mixin is a different type of structure, wich can only be used with the keyword with and is used to include common code snippets, I'd say, reuse the code.
+
+class Animal {}
+
+// behaviors
+abstract class Flyer {
+  void fly() => print('I can fly!');
+}
+
+abstract class Swimmer {
+  void swim() => print('I can swim!');
+}
+
+//using implements... you cannot reuse the code ... in this case you gotta go with 'WITH'
+// class Bird extends Animal implements Flyer {
+//   @override
+//   void fly() => print('I can fly!');
+// }
+
+// class Duck extends Animal implements Swimmer, Flyer {
+//   @override
+//   void fly() => print('I can fly!');
+
+//   @override
+//   void swim() => print('I can swim!');
+// }
+
+class Bird extends Animal with Flyer {}
+
+class Duck extends Animal with Swimmer, Flyer {}
