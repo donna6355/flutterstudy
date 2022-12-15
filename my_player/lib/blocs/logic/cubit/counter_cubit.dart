7543bcafc8470abbd943042ac59275ec86b4008f
Donna <donnajun1214@gmail.com>
2022-12-15 09:08:07 +0900

    connect internet cubit to counter cubit by di

--- my_player/lib/blocs/logic/cubit/counter_cubit.dart
+++ my_player/lib/blocs/logic/cubit/counter_cubit.dart
@@ -1,10 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_player/blocs/logic/cubit/internet_cubit.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(0));
+  final InternetCubit internetCubit;
+  late StreamSubscription internetStream;
+  CounterCubit({required this.internetCubit}) : super(CounterState(0)) {
+    internetStream = internetCubit.stream.listen((InternetState event) {
+      if (event is InternetStatus && event.status == InternetConnection.none) {
+        decrement();
+      } else if (event is InternetStatus) {
+        increment();
+      }
+    });
+  }
 
   void increment() => emit(CounterState(state.counterValue + 1));
   void decrement() => emit(CounterState(state.counterValue - 1));
+
+  @override
+  Future<void> close() {
+    internetStream.cancel();
+    return super.close();
+  }
 }
