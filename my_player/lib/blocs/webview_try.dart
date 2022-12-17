ffbbd13d4357c070c58b2cd1f2e6bccaea672fad
Donna <donnajun1214@gmail.com>
2022-12-17 19:37:16 +0900

    inappwebview message channel

--- my_player/lib/blocs/webview_try.dart
+++ my_player/lib/blocs/webview_try.dart
@@ -17,6 +17,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late WebMessagePort portToJs;
+  late InAppWebViewController webViewController;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,74 +27,38 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(title: Text("JavaScript Handlers")),
           body: SafeArea(
               child: Column(children: <Widget>[
-            Expanded(
-              child: InAppWebView(
-                initialData: InAppWebViewInitialData(data: """
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    </head>
-    <body>
-        <h1>JavaScript Handlers</h1>
-        <script>
-            window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-                window.flutter_inappwebview.callHandler('handlerFoo')
-                  .then(function(result) {
-                    // print to the console the data coming
-                    // from the Flutter side.
-                    console.log(JSON.stringify(result));
+            InAppWebView(
+              onWebViewCreated: (controller) async {
+                webViewController = controller;
+                var hm = await controller.getOptions();
+                print(hm?.crossPlatform.userAgent);
+              },
+              onConsoleMessage: (controller, consoleMessage) {
+                print(
+                    "Message coming from the Dart side: ${consoleMessage.message}");
+              },
+              onLoadStop: (controller, url) async {
+                if (await AndroidWebViewFeature.isFeatureSupported(
+                    AndroidWebViewFeature.CREATE_WEB_MESSAGE_CHANNEL)) {
+                  // wait until the page is loaded, and then create the Web Message Channel
+                  var webMessageChannel =
+                      await controller.createWebMessageChannel();
+                  portToJs = webMessageChannel!.port1;
+                  var port2 = webMessageChannel.port2;
 
-                    window.flutter_inappwebview
-                      .callHandler('handlerFooWithArgs', 1, true, ['bar', 5], {foo: 'baz'}, result);
-                });
-            });
-            window.addEventListener("message", (event) => {
-              console.log(event.data);
-            }, false);
-        </script>
-    </body>
-</html>
-                      """),
-                // initialSettings: settings,
-                initialOptions: InAppWebViewGroupOptions(),
-                onWebViewCreated: (controller) {
-                  controller.addJavaScriptHandler(
-                      handlerName: 'handlerFoo',
-                      callback: (args) {
-                        // return data to the JavaScript side!
-                        return {'bar': 'bar_value', 'baz': 'baz_value'};
-                      });
+                  // set the web message callback for the port1
+                  await portToJs.setWebMessageCallback((message) async {
+                    print("Message coming from the JavaScript side: $message");
+                    await portToJs!
+                        .postMessage(WebMessage(data: message! + " and back"));
+                  });
 
-                  controller.addJavaScriptHandler(
-                      handlerName: 'handlerFooWithArgs',
-                      callback: (args) {
-                        print(args);
-                        // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
-                      });
-                },
-                onLoadStop: (controller, url) async {
-                  await controller.evaluateJavascript(source: """
-    window.addEventListener("myCustomEvent", (event) => {
-      console.log(JSON.stringify(event.detail));
-    }, false);
-  """);
-
-                  await Future.delayed(Duration(seconds: 5));
-
-                  controller.evaluateJavascript(source: """
-    const event = new CustomEvent("myCustomEvent", {
-      detail: {foo: 1, bar: false}
-    });
-    window.dispatchEvent(event);
-  """);
-                },
-                onConsoleMessage: (controller, consoleMessage) {
-                  print(consoleMessage);
-                  // it will print: {message: {"foo":1,"bar":false}, messageLevel: 1}
-                },
-              ),
+                  // transfer port2 to the webpage to initialize the communication
+                  await controller.postWebMessage(
+                    message: WebMessage(data: "capturePort", ports: [port2]),
+                  );
+                }
+              },
             ),
           ]))),
     );
