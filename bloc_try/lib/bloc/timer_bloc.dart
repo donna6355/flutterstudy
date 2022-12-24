2ba3bfcfab8ae8b19ec1b089093739674d781490
Donna <donnajun1214@gmail.com>
2022-12-24 17:00:18 +0900

    timerbloc...hm...

--- bloc_try/lib/bloc/timer_bloc.dart
+++ bloc_try/lib/bloc/timer_bloc.dart
@@ -1,6 +1,5 @@
-// https://bloclibrary.dev/#/fluttertimertutorial
-
 import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import '../ticker.dart';
@@ -9,15 +8,25 @@ part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(const TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
+    on<TimerReset>(_onReset);
+    on<_TimerTicked>(_onTicked);
+  }
+
   final Ticker _ticker;
   static const int _duration = 60;
 
   StreamSubscription<int>? _tickerSubscription;
 
-  TimerBloc({required Ticker ticker})
-      : _ticker = ticker,
-        super(TimerInitial(_duration)) {
-    on<TimerStarted>(_onStarted);
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
   }
 
   void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
@@ -25,12 +34,33 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
     _tickerSubscription?.cancel();
     _tickerSubscription = _ticker
         .tick(ticks: event.duration)
-        .listen((duration) => add(TimerTicked(duration: duration)));
+        .listen((duration) => add(_TimerTicked(duration: duration)));
   }
 
-  @override
-  Future<void> close() {
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onResumed(TimerResumed resume, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
+  }
+
+  void _onReset(TimerReset event, Emitter<TimerState> emit) {
     _tickerSubscription?.cancel();
-    return super.close();
+    emit(const TimerInitial(_duration));
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(
+      event.duration > 0
+          ? TimerRunInProgress(event.duration)
+          : const TimerRunComplete(),
+    );
   }
 }
