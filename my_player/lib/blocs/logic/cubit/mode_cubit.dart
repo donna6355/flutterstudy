9fef97fd6e7ddbc4d9dde760d67bf36d6f46a822
Donna <donnajun1214@gmail.com>
2022-12-29 23:00:56 +0900

    cubit more

---
+++ my_player/lib/blocs/logic/cubit/mode_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'mode_state.dart';
+
+class ModeCubit extends Cubit<ModeState> {
+  ModeCubit() : super(InitialMode());
+
+  void disableMode() => emit(UnavailableMode());
+  void enterAdminMode() => emit(AdminMode());
+}
---
+++ my_player/lib/blocs/logic/cubit/mode_state.dart
@@ -0,0 +1,9 @@
+part of 'mode_cubit.dart';
+
+abstract class ModeState {}
+
+class InitialMode extends ModeState {}
+
+class UnavailableMode extends ModeState {}
+
+class AdminMode extends ModeState {}
