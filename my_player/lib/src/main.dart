19bdba64bb6d397d2abe72b422c0b3dd7226f467
Donna <donnajun1214@gmail.com>
2023-01-06 08:46:24 +0900

    install apk silently package...

--- my_player/lib/src/main.dart
+++ my_player/lib/src/main.dart
@@ -1,6 +1,7 @@
 // https://booiljung.github.io/technical_articles/flutter/state_management/architecture_your_flutter_project_using_bloc_pattern.html
 
 import 'package:flutter/material.dart';
+import 'package:flutter_install_apk_silently/flutter_install_apk_silently.dart';
 import './app.dart';
 
 void main() {
@@ -70,6 +71,33 @@ class _CounterState extends State<Counter> {
 
 class Testless extends StatelessWidget {
   const Testless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// https://pub.dev/packages/flutter_install_apk_silently
+
+class InstallTest extends StatefulWidget {
+  const InstallTest({super.key});
+
+  @override
+  State<InstallTest> createState() => _InstallTestState();
+}
+
+class _InstallTestState extends State<InstallTest> {
+  @override
+  void initState() {
+    super.initState();
+    installAndReboot();
+  }
+
+  Future<void> installAndReboot() async {
+    // bool result = await FlutterInstallApkSilently.installAPK(file: file);
+    bool? result = await FlutterInstallApkSilently.rebootDevice();
+  }
 
   @override
   Widget build(BuildContext context) {
