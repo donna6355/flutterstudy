63e8c55a70d22f6c6542efc40a491bec00a2f4f7
Donna <donnajun1214@gmail.com>
2023-01-14 19:44:41 +0900

    group anagram

---
+++ algorithm/lib/algorithm/04_group_anagram.dart
@@ -0,0 +1,30 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution {
+  List<List<String>> groupAnagrams(List<String> strs) {
+    //declare map for index and list for result; map has comparable str as key and index of res list as value
+    //sort string in alphabetical order
+    //check if sorted string is included in the map's key
+    //if it has, add origin string in the list at the index provided by map's value
+    //if not, add new list to the res list and add map with the comparable str as key and the length of map as the index
+    //O(n)
+
+    Map<String, int> index = {};
+    List<List<String>> res = [];
+
+    for (var str in strs) {
+      final List<String> strList = str.split("")..sort();
+      final String sortedStr = strList.toString();
+      if (index.containsKey(sortedStr)) {
+        res[index[sortedStr]!].add(str);
+        break;
+      }
+      res.add([str]);
+      index.addAll({sortedStr: index.length});
+    }
+
+    return res;
+  }
+}
