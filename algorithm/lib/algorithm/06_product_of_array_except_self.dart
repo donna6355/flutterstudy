ead3a10c8991945347d1f10d99e0e2820b90eeb9
Donna <donnajun1214@gmail.com>
2023-01-17 10:13:19 +0900

    solved product of array except self

--- algorithm/lib/algorithm/06_product_of_array_except_self.dart
+++ algorithm/lib/algorithm/06_product_of_array_except_self.dart
@@ -11,21 +11,41 @@ class Solution {
     // how to get left or right product in O(n)? put product in the new array and multiply next el of nums by prev product
 
     final int numsLen = nums.length;
-    final List<int> lefts = List.generate(numsLen, (_) => 1);
-    final List<int> rights = List.generate(numsLen, (_) => 1);
-    final List<int> res = List.generate(numsLen, (_) => 1);
+    final List<int> lefts = List.generate(numsLen, (index) => 1);
+    final List<int> rights = List.generate(numsLen, (index) => 1);
+    final List<int> res = [];
 
     for (var i = 0; i < numsLen; i++) {
-      if (i != 0) lefts[i] = nums[i] * lefts[i - 1];
+      if (i != 0) lefts[i] = nums[i - 1] * lefts[i - 1];
     }
+    // print(lefts);
 
     for (var i = numsLen - 1; i >= 0; i--) {
-      if (i != numsLen - 1) rights[i] = nums[i] * rights[i + 1];
+      if (i != numsLen - 1) rights[i] = nums[i + 1] * rights[i + 1];
     }
+    // print(rights);
 
     for (var i = 0; i < numsLen; i++) {
-      res[1] = lefts[i] * rights[i];
+      res.add(lefts[i] * rights[i]);
     }
     return res;
+  }
+
+  List<int> productExceptSelfOther(List<int> nums) {
+    final result = List<int>.generate(nums.length, (_) => 1);
+
+    var prefix = 1;
+    for (int i = 0; i < nums.length; i++) {
+      result[i] *= prefix;
+      prefix *= nums[i];
+    }
+
+    var postfix = 1;
+    for (int i = nums.length - 1; i >= 0; i--) {
+      result[i] *= postfix;
+      postfix *= nums[i];
+    }
+
+    return result;
   }
 }
