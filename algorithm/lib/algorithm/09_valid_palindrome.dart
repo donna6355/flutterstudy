512db1e4cb7d0140fefadc5f2b9000aab0a29e85
Donna <donnajun1214@gmail.com>
2023-01-21 21:27:18 +0900

    two pointer palindrome

--- algorithm/lib/algorithm/09_valid_palindrome.dart
+++ algorithm/lib/algorithm/09_valid_palindrome.dart
@@ -5,11 +5,28 @@
 class Solution {
   bool isPalindrome(String s) {
     //convert lowercase and remove whitespace and non alpha-numeric using regExp
-    //split string and reverse
-    //compare if they are same
+    //palindrome means the first half is same to the last half
+    //set two pointer, one from the first and the other from the last
+    //compare first and last and if they are same, increment/decrement pointers until it meets at the center
 
     final String removed = s.toLowerCase().replaceAll(RegExp('[^a-z0-9]'), "");
-    final String reverse = removed.split('').reversed.join('');
-    return removed == reverse;
+    // final String reverse = removed.split('').reversed.join('');
+    // return removed == reverse;
+
+    for (int i = 0, j = removed.length - 1; i < j; i++, j--) {
+      if (removed[i] != removed[j]) return false;
+    }
+    return true;
+  }
+
+  bool isPalindromeOther(String s) {
+    s = s.toLowerCase();
+    var rx = RegExp("[^a-zA-Z0-9]");
+    for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+      while (rx.hasMatch(s[i]) && i < j) i++;
+      while (rx.hasMatch(s[j]) && i < j) j--;
+      if (s[i] != s[j]) return false;
+    }
+    return true;
   }
 }
