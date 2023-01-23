c1cd339eecb6662fdcf88f7425d05fa74147ed30
Donna <donnajun1214@gmail.com>
2023-01-23 17:58:10 +0900

    3sum the otherway

--- algorithm/lib/algorithm/10_3sum.dart
+++ algorithm/lib/algorithm/10_3sum.dart
@@ -37,4 +37,35 @@ class Solution {
     } while (i < numsLen - 2);
     return res;
   }
+
+  List<List<int>> threeSumAnother(List<int> nums) {
+    nums.sort();
+    List<List<int>> list = [];
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      // avoid duplicates
+      if (i > 0 && (nums[i] == nums[i - 1])) continue;
+
+      for (int j = i + 1, k = nums.length - 1; j < k;) {
+        if (nums[i] + nums[j] + nums[k] == 0) {
+          list.add(List.from([nums[i], nums[j], nums[k]]));
+          j++;
+          k--;
+          // avoid duplicates
+          while ((j < k) && (nums[j] == nums[j - 1])) {
+            j++;
+          }
+          // avoid duplicates
+          while ((j < k) && (nums[k] == nums[k + 1])) {
+            k--;
+          }
+        } else if (nums[i] + nums[j] + nums[k] > 0) {
+          k--;
+        } else {
+          j++;
+        }
+      }
+    }
+    return list;
+  }
 }
