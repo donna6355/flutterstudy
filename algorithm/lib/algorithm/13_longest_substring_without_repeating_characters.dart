616c026e2ac59c94daa0792a076fca2901543819
Donna <donnajun1214@gmail.com>
2023-01-24 16:09:47 +0900

    longest substing without repeating characters

---
+++ algorithm/lib/algorithm/13_longest_substring_without_repeating_characters.dart
@@ -0,0 +1,50 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+class Solution {
+  //WRONG WAY
+  // int lengthOfLongestSubstring(String s) {
+  //   //convert to list with each letter
+  //   //check if n+1 string is different using contains?
+  //   //if diff, increment one to resLen
+  //   //if same, change subString
+  //   final List<String> sList = s.split('');
+  //   String subString = sList[0]; //abc
+  //   int resLen = 1; //3
+
+  //   //"pwwkew"
+
+  //   for (var i = 1; i < sList.length - 1; i++) {
+  //     //4
+  //     if (sList[i - 1] == sList[i]) {
+  //       subString = sList[i];
+  //     } else if (!subString.contains(sList[i])) {
+  //       subString = s.substring(i - resLen, i + 1);
+  //       resLen += 1;
+  //     } else {
+  //       subString = s.substring(i - resLen, i + 1);
+  //     }
+  //   }
+  //   return resLen > subString.length ? resLen : subString.length;
+  // }
+
+  int lengthOfLongestSubstring(String s) {
+    //
+    int max = 0;
+    String subStr = '';
+    for (int i = 0; i < s.length; i++) {
+      if (!subStr.contains(s[i])) {
+        subStr += s[i];
+      } else {
+        if (max < subStr.length) {
+          max = subStr.length;
+        }
+        i = i - subStr.length;
+        subStr = '';
+      }
+    }
+    if (subStr.length > max) {
+      max = subStr.length;
+    }
+    return max;
+  }
+}
