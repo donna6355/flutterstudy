6fa5d33cf0b0b8df426244a3a3c54ee9c5541820
Donna <donnajun1214@gmail.com>
2023-01-27 21:42:12 +0900

    min window substing but some bug...

--- algorithm/lib/algorithm/15_minimum_window_substring.dart
+++ algorithm/lib/algorithm/15_minimum_window_substring.dart
@@ -7,13 +7,41 @@ class Solution {
   String minWindow(String s, String t) {
     //length of t is greater than s, return empty string
     //if s == t, return s
-    //set the window of t length, and check all letters are contained?
+    //set the window starting from t length, and check all letters are contained?
 
     final int tLen = t.length;
     final int sLen = s.length;
+    int winSize = tLen;
 
     if (s == t) return s;
     if (tLen > sLen) return '';
-    for (var i = 0; i < sLen; i++) {}
+    for (var i = 0; i <= sLen - winSize; i++) {
+      final String sub = s.substring(i, i + winSize);
+      bool contained = true;
+      contained = containLetter(sub, t);
+      for (var j = 0; j < tLen; j++) {
+        if (!sub.contains(t[j])) {
+          contained = false;
+          break;
+        }
+      }
+      if (contained) return sub;
+      if (i == (sLen - winSize)) {
+        i = 0;
+        winSize += 1;
+      }
+    }
+
+    return containLetter(s, t) ? s : '';
+  }
+
+  bool containLetter(String str, String comp) {
+    for (var j = 0; j < comp.length; j++) {
+      if (!str.contains(comp[j])) {
+        return false;
+      }
+      str = str.replaceFirst(comp[j], "");
+    }
+    return true;
   }
 }
