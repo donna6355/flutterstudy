b011989b278536dd8f6b35771224b0e99dc1aa4e
Donna <donnajun1214@gmail.com>
2023-02-03 08:33:24 +0900

    dart records

--- algorithm/lib/dart/multiple_return.dart
+++ algorithm/lib/dart/multiple_return.dart
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=KhYTFglbF2k
+
 class MultiReturn{
   (String,int) userInfo(Map<String,dynamic> json){
     return (json['name'] as String,json['age'as int]);
@@ -6,8 +8,12 @@ class MultiReturn{
   var json = {'name':'Isaac','age':2};
 
   void test(){
-    var info = userInfo(json);
-    var name = info.$0;
-    var age = info.$1;
+    // var info = userInfo(json);
+    // var name = info.$0;
+    // var age = info.$1;
+
+    //destructuring
+    var (name,age) = userInfo(json);
+
   }
 }
