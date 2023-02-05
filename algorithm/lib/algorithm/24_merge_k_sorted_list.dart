636ee02de64a4accb1211758e06feb87cddf0b55
Donna <donnajun1214@gmail.com>
2023-02-06 08:23:48 +0900

    problem of merge sorted list

---
+++ algorithm/lib/algorithm/24_merge_k_sorted_list.dart
@@ -0,0 +1,13 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class Solution {
+  ListNode? mergeKLists(List<ListNode?> lists) {}
+}
