d8e99950839e40278cf13a437f52f6a3eebfbfc9
Donna <donnajun1214@gmail.com>
2023-02-08 08:03:41 +0900

    merge k sorted list

--- algorithm/lib/algorithm/24_merge_k_sorted_list.dart
+++ algorithm/lib/algorithm/24_merge_k_sorted_list.dart
@@ -9,5 +9,32 @@ class ListNode {
 }
 
 class Solution {
-  ListNode? mergeKLists(List<ListNode?> lists) {}
+  ListNode? mergeKLists(List<ListNode?> lists) {
+    //if lists is empty return null
+    //if lists length is 1 return first listNode
+    //merge two list first and then next two...
+
+    if (lists.isEmpty) return null;
+    if (lists.length == 1) return lists[0];
+    return lists.reduce(mergeTwoLists);
+  }
+
+  ListNode? mergeTwoLists(ListNode? l1, ListNode? l2) {
+    ListNode? l3 = ListNode();
+    ListNode? curr = l3;
+
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        curr?.next = l1;
+        l1 = l1.next;
+      } else {
+        curr?.next = l2;
+        l2 = l2.next;
+      }
+      curr = curr?.next;
+    }
+
+    curr?.next = l1 ?? l2;
+    return l3.next;
+  }
 }
