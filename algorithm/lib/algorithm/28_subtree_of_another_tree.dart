e2c6b3dac18cb43c62c415b571eacab431d80e14
Donna <donnajun1214@gmail.com>
2023-02-10 20:18:38 +0900

    subtree of another tree

--- algorithm/lib/algorithm/28_subtree_of_another_tree.dart
+++ algorithm/lib/algorithm/28_subtree_of_another_tree.dart
@@ -17,6 +17,18 @@ class TreeNode {
 
 class Solution {
   bool isSubtree(TreeNode? root, TreeNode? subRoot) {
-    //
+    //check if root is null then return subRoot is null or not;
+    //reculsively compare val
+    // if val is diff, check subroot with left then right
+    if (root == null) return subRoot == null;
+    return isEqual(root, subRoot) ||
+        isSubtree(root.left, subRoot) ||
+        isSubtree(root.right, subRoot);
+  }
+
+  bool isEqual(TreeNode? root1, TreeNode? root2) {
+    if (root1 == null || root2 == null) return root1 == null && root2 == null;
+    if (root1.val != root2.val) return false;
+    return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
   }
 }
