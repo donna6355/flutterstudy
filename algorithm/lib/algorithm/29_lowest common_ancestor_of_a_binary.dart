b5cf57b78bd203088c14ad0c44696d4a0e7bbc2f
Donna <donnajun1214@gmail.com>
2023-02-10 20:25:04 +0900

    add descriptions

---
+++ algorithm/lib/algorithm/29_lowest common_ancestor_of_a_binary.dart
@@ -0,0 +1,22 @@
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Slution {}
