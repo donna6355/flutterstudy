0f7ab4f61cf6cfaa5ea5cc81f7e5477428be5388
Donna <donnajun1214@gmail.com>
2023-02-16 10:04:46 +0900

    as bst is sorted

---
+++ algorithm/lib/algorithm/32_kth_smallest_element_in_a_bst.dart
@@ -0,0 +1,47 @@
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Solution {
+  int kthSmallest(TreeNode? root, int k) {
+    //add all element in an array
+    //sort array
+    //return kth element
+
+    final List<int> list = [];
+    addElement(list, root);
+    list.sort();
+    return list[k - 1];
+  }
+
+  void addElement(List<int> list, TreeNode? node) {
+    if (node == null) return;
+    list.add(node.val);
+    addElement(list, node.right);
+    addElement(list, node.left);
+  }
+
+  int kthSmallestOther(TreeNode? root, int k) {
+    int ans = -1;
+    f(TreeNode? root) {
+      if (root == null) {
+        return;
+      }
+      f(root.left);
+      k--;
+      if (k == 0) {
+        ans = root.val;
+        return;
+      }
+      f(root.right);
+    }
+
+    f(root);
+    return ans;
+  }
+}
