6c191676c9c8191e2c83aeb704fbd357bfa0d92c
Donna <donnajun1214@gmail.com>
2023-02-19 21:39:47 +0900

    serialize and deserialize

--- algorithm/lib/algorithm/35_serialize_and_deserialize_binary_tree.dart
+++ algorithm/lib/algorithm/35_serialize_and_deserialize_binary_tree.dart
@@ -22,5 +22,62 @@ class TreeNode {
 }
 
 class Solution {
-  int serializeAndDeserialize(TreeNode? root) {}
+  String serialize(TreeNode? root) {
+    //find preorder and inorder
+    //convert list into string and concat two string with the divider "T"
+    if (root == null) return '';
+    final List<int?> preO = preorderTraversal(root);
+    final List<int?> inO = inorderTraversal(root);
+
+    return preO.join(',') + inO.join(',');
+  }
+
+  inorderTraversal(TreeNode? root) {
+    const result = [];
+
+    void traverse(TreeNode? root) {
+      if (root == null) return;
+
+      traverse(root.left);
+      result.add(root.val);
+      traverse(root.right);
+    }
+
+    traverse(root);
+    return result;
+  }
+
+  List<int?> preorderTraversal(TreeNode? root) {
+    List<int?> rootIsEmpty = root != null ? [root.val] : [];
+    List<int?> rootLeft =
+        root?.left != null ? preorderTraversal(root?.left) : [];
+    List<int?> rootRight =
+        root?.right != null ? preorderTraversal(root?.right) : [];
+    return rootIsEmpty + rootLeft + rootRight;
+  }
+
+  TreeNode? deserialize(String data) {
+    //divid string into two by "T"
+    //convert string into list;
+    if (data.isEmpty) return null;
+    int idx = data.indexOf('T');
+    String preS = data.substring(0, idx);
+    String inS = data.substring(idx + 1);
+
+    List<String> preList = preS.split(',');
+    List<String> inList = inS.split(',');
+
+    return buildTree(preList, inList);
+  }
+
+  TreeNode? buildTree(List<String> preorder, List<String> inorder) {
+    if (preorder.isEmpty || inorder.isEmpty) return null;
+
+    final TreeNode root = TreeNode(int.parse(preorder[0]));
+    final int idx = inorder.indexOf(preorder[0]);
+    root.left =
+        buildTree(preorder.sublist(1, idx + 1), inorder.sublist(0, idx));
+    root.right = buildTree(preorder.sublist(idx + 1), inorder.sublist(idx + 1));
+    return root;
+  }
 }
