4b699217f236f080391d968b35b8e3f59c884336
Donna <donnajun1214@gmail.com>
2023-02-20 14:03:19 +0900

    trie...

---
+++ algorithm/lib/algorithm/36_implement_trie.dart
@@ -0,0 +1,26 @@
+// A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+// Implement the Trie class:
+
+// Trie() Initializes the trie object.
+// void insert(String word) Inserts the string word into the trie.
+// boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+// boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+class Trie {
+  Trie() {}
+
+  void insert(String word) {}
+
+  bool search(String word) {}
+
+  bool startsWith(String prefix) {}
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = Trie();
+ * obj.insert(word);
+ * bool param2 = obj.search(word);
+ * bool param3 = obj.startsWith(prefix);
+ */
