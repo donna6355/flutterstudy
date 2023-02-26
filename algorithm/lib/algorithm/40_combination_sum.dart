5537e2623d99fb57bc1581393ceb2fa25129ec42
Donna <donnajun1214@gmail.com>
2023-02-26 10:42:28 +0900

    recursive combination sum

--- algorithm/lib/algorithm/40_combination_sum.dart
+++ algorithm/lib/algorithm/40_combination_sum.dart
@@ -7,68 +7,28 @@
 // The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
 
 class Solution {
-  List<List<int>> combinationSum(List<int> candidates, int target) {}
+  List<List<int>> combinationSum(List<int> candidates, int target) {
+    if (candidates.isEmpty) return [];
+    if (target == 0) return [[]];
+    candidates.sort();
+    List<List<int>> paths = [];
+
+    void find(int target, List<int> path, int idx) {
+      if (target == 0) {
+        paths.add(path);
+        return;
+      } else if (target < 0) {
+        return;
+      } else {
+        while (idx < candidates.length && target - candidates[idx] >= 0) {
+          find(target - candidates[idx], [...path, candidates[idx]], idx);
+          idx++;
+        }
+      }
+    }
+
+    find(target, [], 0);
+
+    return paths;
+  }
 }
-
-// var combinationSum = function(candidates, target) {
-
-//     // No candidates, means no results
-//     if (!candidates) {
-//         return [];
-//     }
-
-//     // Empty set is the only solution for target 0
-//     if (target === 0) {
-//         return [[]];
-//     }
-
-//     // Sort the candidates array as otherwise we could
-//     // come up with solution [3,2,2] instead of [2,2,3]
-//     candidates.sort((a,b) => { return a - b});
-
-//     // Store all possible combinations in here
-//     let paths = [];
-
-//     // The recursive part.
-//     // t is what we're looking for. This will become smaller, deeper in to the recursive calls
-//     // p is where we will record our current path
-//     // i is the index of the numbers we're considering. Once we get stuck with the 2's
-//     // we will increase i to try other combinations
-
-//     let find = function (t, p, i) {
-
-//         // check std out to  get a feel for the order in which we encounter 2,3,6,7
-//         console.log('considering:', t , p, i);
-
-//         if (t === 0) {
-//             // we found a valid path, so store that in the paths.
-//             paths.push(p);
-//             return;
-//         } else if (t < 0) {
-//             console.log('Dead path. Will our code actually ever get here?');
-//             return;
-//         } else {
-//             // don't run over the candidates array length
-//             // && don't try candidates that would bring target below 0
-//             while (i < candidates.length && t - candidates[i] >= 0) {
-
-//                 // "Use" candidate[i]: Lower our target, and record the candidate in the path
-//                 // We're cloning the path array, or it will contaminate future paths.
-//                 find(t - candidates[i], [...p, candidates[i]], i)
-
-//                 // "Lose" candidate[i]:
-//                 // In our main example, we don't hit this path until the path of pure 2's
-//                 // has been tried and found to lead to [2,2,2] with no candidates worth pursuing                 // further, because of the 2nd check of the while loop condition.
-//                 i++;
-//             }
-
-//         }
-
-//     }
-
-//     // kick off initial case, we're looking for the original target,
-//     // our current path is empty, and we'll consider all candidates
-//     find (target, [], 0);
-
-//     return paths;
-// };
