24252020c1c77c523e006dd0d603d275094e5d9a
Donna <donnajun1214@gmail.com>
2023-03-07 09:02:16 +0900

    alien dictionary problem'

---
+++ algorithm/lib/algorithm/48_alien_dictionary.dart
@@ -0,0 +1,5 @@
+// There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+
+class Solution {
+  String alienOrder(List<String> words) {}
+}
