760bf97d5e2f04f8a0b4a2a03db2b81edc25e64f
Donna <donnajun1214@gmail.com>
2023-03-26 19:23:13 +0900

    constant gap widgets

---
+++ ui_study/lib/gap.dart
@@ -0,0 +1,9 @@
+// gaps.dart
+import 'package:flutter/material.dart';
+
+const gapW4 = SizedBox(width: 4);
+const gapW8 = SizedBox(width: 8);
+const gapW12 = SizedBox(width: 12);
+const gapW64 = SizedBox(width: 64);
+
+// your code will be more performant because all the gaps above are hardcoded as compile-time constants.
