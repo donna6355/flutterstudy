0d9bbb27e509b8ba386c228aa9f558f12eb3e415
Donna <donnajun1214@gmail.com>
2023-04-08 09:48:02 +0900

    key in stateless and stateful

---
+++ ui_study/lib/key_sample.dart
@@ -0,0 +1,127 @@
+// keys are required for modifying widgets only if they are stateful. Stateless widgets don't require them.
+
+import 'package:flutter/material.dart';
+
+class PositionTiles extends StatefulWidget {
+  const PositionTiles({Key? key}) : super(key: key);
+  @override
+  State<PositionTiles> createState() => _PositionTilesState();
+}
+
+class _PositionTilesState extends State<PositionTiles> {
+  List<Widget> tiles = [];
+  @override
+  void initState() {
+    super.initState();
+    tiles = [
+      StatelessColorTiles(),
+      StatelessColorTiles(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: tiles,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.sentiment_very_satisfied,
+        ),
+        onPressed: swapTiles,
+      ),
+    );
+  }
+
+  void swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
+}
+
+class StatelessColorTiles extends StatelessWidget {
+  Color myColor = Colors.black38;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: myColor,
+    );
+  }
+}
+
+class PositionTilesTwo extends StatefulWidget {
+  const PositionTilesTwo({Key? key}) : super(key: key);
+  @override
+  State<PositionTilesTwo> createState() => _PositionTilesTwoState();
+}
+
+class _PositionTilesTwoState extends State<PositionTilesTwo> {
+  List<Widget> tiles = [];
+  @override
+  void initState() {
+    super.initState();
+    tiles = [
+      StatefulColorTiles(
+        key: UniqueKey(),
+      ),
+      StatefulColorTiles(
+        key: UniqueKey(),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: tiles,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.sentiment_very_satisfied,
+        ),
+        onPressed: swapTiles,
+      ),
+    );
+  }
+
+  void swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
+}
+
+class StatefulColorTiles extends StatefulWidget {
+  const StatefulColorTiles({Key? key}) : super(key: key);
+  @override
+  State<StatefulColorTiles> createState() => _StatefulColorTilesState();
+}
+
+class _StatefulColorTilesState extends State<StatefulColorTiles> {
+  late Color myColor;
+  @override
+  void initState() {
+    super.initState();
+    myColor = Colors.red;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: myColor,
+    );
+  }
+}
