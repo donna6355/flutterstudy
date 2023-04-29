95beaf9de5b9b10b7a57578d334275e5839e23b5
Donna <donnajun1214@gmail.com>
2023-04-29 17:36:51 +0900

    [remove] unneccessary parts

--- ui_study/windows/flutter/generated_plugin_registrant.cc
+++ ui_study/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <media_kit_video/media_kit_video_plugin_c_api.h>
-#include <video_player_win/video_player_win_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   MediaKitVideoPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
-  VideoPlayerWinPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("VideoPlayerWinPluginCApi"));
 }
--- ui_study/windows/flutter/generated_plugins.cmake
+++ ui_study/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_video
-  video_player_win
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
