c74c7c1a7eeeac113e0f47d33d7ed9ed735e548a
Donna <donnajun1214@gmail.com>
2023-05-01 14:13:34 +0900

    animated text kit sample

--- ui_study/lib/main.dart
+++ ui_study/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
 import './media_kit_sample.dart';
@@ -78,6 +79,22 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
+              AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'Hello world!',
+                    textStyle: const TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    speed: const Duration(milliseconds: 2000),
+                  ),
+                ],
+                totalRepeatCount: 4,
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              )
             ],
           )),
       floatingActionButton: FloatingActionButton(
