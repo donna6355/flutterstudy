5202b53b7997dd3634a68c83b015ccb3acdc6b8b
Donna <donnajun1214@gmail.com>
2023-05-20 23:01:35 +0900

    dart io

--- ui_study/lib/file_io.dart
+++ ui_study/lib/file_io.dart
@@ -3,8 +3,12 @@ import 'dart:io';
 class LogController {
   final File file = File('sample/counter.txt');
 
-  void writeFile() {
+  Future<void> writeFile() async {
     file.writeAsString('abcde_fghij_klmno_pqrst_uvwxy_z',
         mode: FileMode.append);
+  }
+
+  Future<void> readFile() async {
+    String readData = await file.readAsString();
   }
 }
